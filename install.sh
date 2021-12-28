afff2492ed055ac3cab6c475781bb0701f671693
Dimassss <dimas.karpusss@gmail.com>
2021-12-28 05:53:33 +0000

    added some pkg-es to install

--- install.sh
+++ install.sh
@@ -30,7 +30,11 @@ utils=$h/home/utils
 cp -r ./* $utils/
 
 #basic programs install
-sudo pacman -S xorg xorg-xinit xorg-setxkbmap ttf-dejavu nvidia nvidia-utils xf86-video-intel mesa alsa-utils rxvt-unicode chromium base-devel
+sudo pacman -S xorg xorg-xinit xorg-setxkbmap ttf-dejavu 
+sudo pacman -S nvidia nvidia-utils xf86-video-intel mesa 
+sudo pacman -S alsa-utils rxvt-unicode chromium base-devel
+sudo pacman -S chromium feh git python3 r rar gzip telegram-desktop
+sudo pacman -S xclip zathura zathura-pdf-poppler
 
 #i3 install
 sudo pacman -S i3 i3-gaps i3status i3blocks dmenu xterm
